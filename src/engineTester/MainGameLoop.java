4c048e3533c6c3f90b17a01c48de4f911639a2ba
Vishnu Prasanth <mail.vishnupk@gmail.com>
2018-01-26 13:01:24 +0530

    rendered a white rectangle on black background using VAOs and VBOs

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,6 +3,9 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -10,12 +13,30 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				// left bottom triangle
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				// right top triangle
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		while(!Display.isCloseRequested()) {
-			// game logic
+			renderer.prepare();
 			// render
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
 		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 		
 	}
